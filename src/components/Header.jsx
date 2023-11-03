af0cf9be87cdf2574d9eddfbf246f05438353bd7
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-11-04 00:00:14 +0600

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,33 @@
+import { Disclosure,} from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+
 function Header() {
   
   return (
+
+    <Disclosure as="nav" className="bg-gray-800">
+        {({ open }) => (
+
+
+
     <header className="container mx-auto">
       <nav className="border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" className="flex items-center">
-          <h1 className="text-6xl font-bold text-white">Devloper <span className="text-[#ff004f]">R.</span></h1>
+          <h1 className="xl:text-6xl font-bold text-white lg:text-4xl">Devloper <span className="text-[#ff004f]">R.</span></h1>
           </a>
+
+          <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li className="px-[10px] py-[5px] bg-[#262626] rounded-md drop-shadow-[0_0_8px_rgba(255,255,255,1)] hover:drop-shadow-[0_0_8px_rgba(255,0,79,1)] hover:text-[#ff004f] text-white">
@@ -29,7 +50,9 @@ function Header() {
         </div>
       </nav>
     </header>
-  )
+        )}  
+   </Disclosure>
+   )
 }
 
 export default Header;
