66d3cf8c54a279ec3b2e25bdd2295f648c2ade18
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-11-11 00:52:58 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
