6b284cd71a1a31187597f3200d56a8debc3f0d7d
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-11-18 23:59:48 +0600

    Material UI add

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,9 @@
 // import {BiLogoJavascript} from "react-icons/Bi";
 // import {FaBootstrap} from "react-icons/Fa";
 // import {BiLogoTailwindCss} from "react-icons/Bi";
+
+
+
 const Services = () => {
   return (
     <div className="max-w-screen-xl px-[10%] sm:px-[25px] lg:px-[15px] mx-auto mt-[10rem]" id="services">
@@ -12,6 +15,9 @@ const Services = () => {
         <div className="col-span-1 hover:translate-y-[-20px] duration-500 cursor-pointer">
           <div className="p-[40px] bg-[#262626] rounded-md drop-shadow-[0_0_8px_rgba(255,255,255,1)] hover:drop-shadow-[0_0_8px_rgba(255,0,79,1)] hover:text-[#ff004f] text-white hover:bg-[#080808] duration-500">
             {/* <HiOutlineCode color='#fff' fontSize='60px'/> */}
+            <span className="material-icons-outlined">
+            code
+            </span>
             <h1 className="text-[30px] font-bold my-5">Web Design</h1>
             <p className="text-[#ababab]">I am a seasoned web designer with a proven track record of crafting visually stunning and intuitive websites. My proficiency in HTML, CSS, and JavaScript allows me to create seamless user experiences. I look forward to the opportunity to contribute to your team&apos;s success.</p>
             <button className="px-[10px] py-[5px] bg-[#262626] rounded-md drop-shadow-[0_0_8px_rgba(255,255,255,1)] hover:drop-shadow-[0_0_8px_rgba(255,0,79,1)] hover:text-[#ff004f] text-white my-5">
